c4ef352ddca6db13e156434781073ff37aa8cb34
Crystal Wu <cw683@cornell.edu>
2020-07-28 19:42:52 -0400

    ok that iddn't work

--- links.js
+++ links.js
@@ -1,3 +1,17 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBm87XbFNx1qbyXw9eBvgovbVXBD5osCpQ",
+  authDomain: "cusail-omnibox-a6705.firebaseapp.com",
+  databaseURL: "https://cusail-omnibox-a6705.firebaseio.com",
+  projectId: "cusail-omnibox-a6705",
+  storageBucket: "cusail-omnibox-a6705.appspot.com",
+  messagingSenderId: "782612915461",
+  appId: "1:782612915461:web:5df3976372f0736f2f436c",
+  measurementId: "G-DLH92W1HM0"
+};
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
+
 const list = document.querySelector('#linktable');
 const form = document.querySelector('#add-link-form');
 const updateForm = document.querySelector('#update-link-form')
--- login.js
+++ login.js
@@ -1,3 +1,17 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBm87XbFNx1qbyXw9eBvgovbVXBD5osCpQ",
+  authDomain: "cusail-omnibox-a6705.firebaseapp.com",
+  databaseURL: "https://cusail-omnibox-a6705.firebaseio.com",
+  projectId: "cusail-omnibox-a6705",
+  storageBucket: "cusail-omnibox-a6705.appspot.com",
+  messagingSenderId: "782612915461",
+  appId: "1:782612915461:web:5df3976372f0736f2f436c",
+  measurementId: "G-DLH92W1HM0"
+};
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
+
 function signIn() {
   var provider = new firebase.auth.GoogleAuthProvider();
   provider.addScope("https://www.googleapis.com/auth/userinfo.email");
